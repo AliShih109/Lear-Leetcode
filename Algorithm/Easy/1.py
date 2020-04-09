e13e1399e45e4dd6f750e77ad62a8c70f6220792
AliShih109 <63403558+AliShih109@users.noreply.github.com>
2020-04-09 22:43:01 +0800

    Update 1.py

--- Algorithm/Easy/1.py
+++ Algorithm/Easy/1.py
@@ -1,3 +1,4 @@
+'''
 1. Two Sum
 
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
@@ -9,3 +10,17 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
+'''
+
+
+class Solution():
+    def twoSum(self, nums, target):
+        if len(nums) <= 1:
+            return False
+        
+        my_dict = {}
+        for i in range(len(nums)):
+            if target - nums[i] in my_dict:
+                return [my_dict[target - nums[i]], i]
+            else:
+                my_dict[nums[i]] = i #用dict就只需掃1次而不是2次
